2afc9e071b458287119dec57be86409733192172
yasmimSM2007 <santosmoreirayasmin54@gmail.com>
2024-08-30 12:37:58 -0300

    30/08

---
+++ .history/src/components/Footer_20240830114124.jsx
@@ -0,0 +1,12 @@
+export default function Footer () {
+return(
+    <footer> 
+        <div class="footer">
+            <h5>Sobre</h5>
+            <p>Fale Conosco</p>
+            <p>Acesse o Instagram</p>
+            <p>Dúvidas Frequentes</p>
+        </div>
+    </footer>
+)
+}
---
+++ .history/src/components/Footer_20240830114152.jsx
@@ -0,0 +1,12 @@
+export default function Footer () {
+return(
+    <footer> 
+        <div class="footer">
+            <h7>Sobre</h7>
+            <p>Fale Conosco</p>
+            <p>Acesse o Instagram</p>
+            <p>Dúvidas Frequentes</p>
+        </div>
+    </footer>
+)
+}
